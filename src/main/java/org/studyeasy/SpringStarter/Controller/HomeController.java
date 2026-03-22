3a7e4bbeae200b32e929bb13bab79c3b984b3cfa
ashish.phiedge <ashish@phiedge.co.in>
2026-03-22 22:32:55 +0530

    Update controller, templates, and add nav background image

--- src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
+++ src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
@@ -6,17 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(Model model){
         return "home";
     }
-    @GetMapping("/about")
-    public String about(Model model){
-        return "about";
-    }
-    @GetMapping("/book")
-    public String book(Model model){
-        return "book";
-    }
+  
     
 }
